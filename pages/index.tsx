bc136ccdedb2b4a1f9396ce8dc00d3300eba50a3
Crystal <dev.sujeong@gmail.com>
2021-04-06 21:14:22 +0900

    [Style] base.scss 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function Home(): ReactNode {
   return (
-    <div className="l-wrapper">
+    <div className="layout-wrapper">
       <h1>test</h1>
       <abbr title="shortlanguage">sl</abbr>
       Mini Project fighting!Mini Project fighting!Mini Project fighting!Mini Project fighting!Mini
