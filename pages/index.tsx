71a86668efff0827c64f8e5353d3ee3c62244801
devSujeong <dev.sujeong@gmail.com>
2021-05-11 21:19:40 +0900

    splash 화면 작업 svg 컴포넌트 구축, 애니메이션 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import Splash from '../components/common/Splash';
 
 function Home(): ReactNode {
+  const [isSplashing, setIsSplashing] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSplashing(false);
+    }, 2000);
+  });
+
   return (
     <>
-      <Splash />
+      {isSplashing && <Splash />}
+      main
     </>
   );
 }
