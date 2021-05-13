e0a50fede15aba16d1aa03f0e04980f9f5cb9b6a
devSujeong <dev.sujeong@gmail.com>
2021-05-13 23:13:53 +0900

    Splash화면을 SessionStorage로 연결
    웹에 처음 접근했을 때만 splash 화면을 실행시키기 위해서 SessionStorage로 연결함

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import { ReactNode, useEffect, useState } from 'react';
 import Splash from '../components/common/Splash';
 
 function Home(): ReactNode {
-  const [isSplashing, setIsSplashing] = useState(true);
+  const [isSplashing, setIsSplashing] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsSplashing(false);
-    }, 2000);
-  });
+    if (!sessionStorage.getItem('initLoading')) {
+      setIsSplashing(true);
+
+      setTimeout(() => {
+        sessionStorage.setItem('initLoading', 'initLoadingCompleted');
+        setIsSplashing(false);
+      }, 2000);
+    }
+  }, []);
 
   return (
     <>
